e499b24d8f73166a337f3483cdf99b52cbfa85eb
Artur Kołodziej <w69798@student.wsiz.edu.pl>
2023-11-05 15:31:29 +0100

    Initial commit

---
+++ Lab 0.py
@@ -0,0 +1,8 @@
+#Zad3
+bok1=int(input("podaj długość boku 1:"))
+bok2=int(input("podaj długość boku 2:"))
+obwód=(bok1*2)+(bok2*2)
+pole=bok1*bok2
+print("pole wynosi: ",pole)
+print("obwód wynosi: ",obwód)
+
