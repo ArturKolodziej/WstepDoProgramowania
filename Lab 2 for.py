238b753636eb5a2c29c2f9582bbf539f8954ef26
ArturKolodziej <artur.kolodziej01@gmail.com>
2023-12-08 19:31:00 +0100

    Aktualizacja repozytorium.

--- Lab 2 for.py
+++ Lab 2 for.py
@@ -38,3 +38,11 @@ r=int(input("Podaj róznicę ciągu: "))
 for i in range(1,n+1):
     an=a+(i-1)*r
     print(an)
+
+#Zad5
+silnia=int(input("Podaj liczbę do wyliczenia silni: "))
+iloczyn = 1
+for i in range(1,silnia+1):
+    iloczyn=iloczyn*i
+    i=i+1
+print(iloczyn)
--- Lab 2 while.py
+++ Lab 2 while.py
@@ -18,7 +18,11 @@ else:
     print(f"Cyfry są równe {c1}={c2}")
 
 #Zad2
-
+i=-4
+while i<=4:
+    y=2*(i*i)-5*i-8
+    print(f"Wartość fumkcji dla x={i}: {y}\n")
+    i=i+0.5
 
 #Zad3
 x=0
